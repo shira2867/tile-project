28042231cdf1ebd1bda5753d54c037a23ce22738
shira2867 <shiragoldstein2867@gmail.com>
2026-01-13 09:34:19 +0200

    fix problem in admin page

--- client/src/routers/route.tsx
+++ client/src/routers/route.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import { AdminPage } from '../pages/AdminPage/AdminPage';
@@ -8,10 +8,12 @@ export const TileRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/tiles" element={<TilePage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
